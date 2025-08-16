6ad261e2cd5dc780bd236205ecffcc61711e7f2c
AMAN SINGH <2amansingh07@gmail.com>
2025-08-16 21:15:58 +0530

    fixed scrollbar showing in sidebar

--- src/app/components/sideBar.tsx
+++ src/app/components/sideBar.tsx
@@ -9,15 +9,16 @@ function SideBar() {
 const [account, setAccount] = useState<string[]>() 
 
 useEffect(()=>{
-    setAccount(data)
+   setAccount(data)
 })
 
 
 
   return (
     <div className="flex flex-col justify-between items-center border-r-2 border-gray-600 w-20 p-2 font-bold font-ubuntu">
-        <div className="overflow-y-scroll ">
- <h3 className="mt-4 underline">Account</h3>
+      <h3 className="mt-4 underline">Account</h3>
+        <div className="overflow-y-scroll  hide-scrollbar">
+ 
       {account?.map((e)=>(
         <div className=" flex flex-col justify-center   w-16 h-16 bg-blue-300  rounded-[100%] text-center mt-4 border-3 border-blue-500 text-black cursor-pointer hover:border-4 hover:border-white">
             {e}
